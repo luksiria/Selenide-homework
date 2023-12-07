ef991be3cc45ac757169a6718e129faccba77a52
luksiria <kristinalacrima@gmail.com>
2023-12-07 22:16:21 +0300

    fin_mb

---
+++ src/main/java/ru/netology/web/CallBackTest.java
@@ -0,0 +1,23 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.Test;
+
+import com.codeborne.selenide.Selenide;
+
+
+
+public class CallBackTest {
+    @Test
+    public void tt(){
+        Selenide.open("http://localhost:9999");
+        Selenide.$("[data-test-id=name] input").setValue("оксана");
+        Selenide.$("[data-test-id=phone] input").setValue("+70000000000");
+        Selenide.$("[data-test-id=agreement]").click();
+        Selenide.$("button").click();
+        Selenide.$("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+
+    }
+}
