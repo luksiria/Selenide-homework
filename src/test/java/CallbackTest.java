d621048a9113722f4e8d6dbf9ac38cfeefba2042
luksiria <kristinalacrima@gmail.com>
2023-12-16 14:46:15 +0300

    changess

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -2,23 +2,33 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.Keys;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selectors.byText;
 
 
 class CallBackTest{
     @Test
     public void tt(){
         java.util.Date date = new java.util.Date();
-        Configuration.headless = true;
+        String planningDate = generateDate(4);
         Selenide.open("http://localhost:9999");
         Selenide.$("[data-test-id=city] input").setValue("Казань");
-        Selenide.$("[data-test-id=date] input").setValue(String.valueOf(date));
+        Selenide.$("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        Selenide.$("[data-test-id=date] input").setValue(planningDate);
         Selenide.$("[data-test-id=name] input").setValue("иван");
         Selenide.$("[data-test-id=phone] input").setValue("+71111111111");
         Selenide.$("[data-test-id=agreement]").click();
-        Selenide.$("#root > div > form > fieldset > div.grid-row.grid-row_gutter-mobile-s_16.grid-row_gutter-desktop-m_24.grid-row_justify_between.grid-row_theme_alfa-on-white > div.grid-col.grid-col_width_3.grid-col_theme_alfa-on-white.grid-col_gutter-mobile-s_16.grid-col_gutter-desktop-m_24 > div > button").click();
-        Selenide.$("#root > div > div > div.notification__title").shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.exactText("Успешно!"));
+        Selenide.$(byText("Забронировать")).click();
+        Selenide.$(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
 
+    }
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 }
